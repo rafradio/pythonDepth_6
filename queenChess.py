3719603a34b3a9e43e64c4c851c6f8ad7739c66f
Rafail Abdyushev <rafaelab@mail.ru>
2023-06-30 10:09:45 +0300

    chess 2

--- queenChess.py
+++ queenChess.py
@@ -7,30 +7,13 @@ def main():
 
         obj = m.QueenPositions()
         obj.initSets()
-        j = 0
         obj.putFirstQueen()
-        j = 1
-        obj.findNewPositions(j)
-        j = 2
-        obj.findNewPositions(j)
-        j = 3
-        
-        # print(obj.chessBoard)
-        obj.findNewPositions(j)
-        j = 4
-        # obj.queensPosition.append('c6')
-        obj.findNewPositions(j)
-        obj.findNewPositions()
-        obj.findNewPositions()
-        obj.findNewPositions()
-        # for i in range(len(obj.chessBoardForMoving)):
-        #     print(obj.chessBoardForMoving[i])
-        if obj.queensPosition[7] != 'h8':
+        for _ in range(7): obj.findNewPositions()
+        if obj.queensPosition[7] != 'a1':
             print(i)
             print(obj.queensPosition)
             break
 
-        # print(obj.queensPosition)
 
 if __name__ == '__main__':
     main()
